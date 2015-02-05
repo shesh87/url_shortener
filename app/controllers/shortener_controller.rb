933998265a358c4a7602baa6a62f3caf10d65c49
Sheonna Harris <svonharris@gmail.com>
2015-02-05 17:35:51 -0500

    Created some routes, create/show/redirect views, and added urls to the database

--- app/controllers/shortener_controller.rb
+++ app/controllers/shortener_controller.rb
@@ -1,5 +1,28 @@
 class ShortenerController < ApplicationController
-	def index
-		render 'index'
+
+	def new
+		@url = Url.new
 	end
+
+	def create
+		@url = Url.new(url_params) 
+		if @url.save
+			redirect_to(url_path(@url))
+		else
+			render "bad"
+		end
+	end
+	
+	def show
+		@url = Url.new(params[:id])
+	end
+
+	def redirect
+		redirect_to(url_path(params[:id]))
+	end
+
+	private
+		def url_params
+			params.require(:url).permit(:current_url)
+		end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,8 @@
 class Url < ActiveRecord::Base
+	validates :current_url, presence: true
+
+	def rando_addy(id)
+	 	new_url = (0...10).map { ('a'..'z').to_a[rand(26)] }.join
+	 	
+	end 
 end
